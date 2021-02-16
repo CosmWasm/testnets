6e6764504d07b47dba4554cd5a3f3de0904d6e60
orkunkl <kulceorkun@gmail.com>
2021-02-17 00:31:41 +0300

    Fix DAEMON_HOME setup in cosmovisor.service

--- devops/node/cosmovisor/services.sh
+++ devops/node/cosmovisor/services.sh
@@ -2,15 +2,14 @@
 set -euo pipefail
 
 # set all variables
-WASMD_HOME="${WASMD_HOME:-/root/.wasmd}"
+APP_HOME="${APP_HOME:-/root/.wasmd}"
 COSMOVISOR_BINARY_DIR=${COSMOVISOR_BINARY_DIR:-/usr/local/bin}
 
 TARGET="/etc/systemd/system/cosmovisor.service"
 
 # copy service file and customize it
 mv ./cosmovisor.service "${TARGET}"
-# use , as separator, as $DOCKER and $WASMD_HOME use / internally
-sed -i "s,WASMD_HOME,$WASMD_HOME,g" "${TARGET}"
+sed -i "s,APP_HOME,$APP_HOME,g" "${TARGET}"
 sed -i "s,COSMOVISOR_BINARY_DIR,$COSMOVISOR_BINARY_DIR,g" "${TARGET}"
 
 systemctl enable cosmovisor.service
