14327060b4d0580f62210fc7bf30e5454d19061c
orkunkl <kulceorkun@gmail.com>
2021-02-17 13:17:16 +0300

    Fix faucet

--- devops/faucet/faucet.sh
+++ devops/faucet/faucet.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ufw allow 22,80,443/tcp
 
 # TODO: configure this better.. this is the bech32 prefix for the chain
-PREFIX=${PREFIX:-coral}
+PREFIX=${PREFIX:-wasm}
 
 # Note: this requires the ../endpoints scripts to have been run locally before
 # and the various deps (certbot, nginx, etc) already set up.
@@ -13,6 +13,6 @@ PREFIX=${PREFIX:-coral}
 
 docker build -t cosmwasm/faucet:manual .
 
-docker run --rm cosmwasm/faucet:manual cosmwasm-faucet version
+docker run --rm cosmwasm/faucet:manual cosmos-faucet version
 
-docker run -e FAUCET_ADDRESS_PREFIX=coral --rm cosmwasm/faucet:manual cosmwasm-faucet generate
+docker run -e FAUCET_ADDRESS_PREFIX=$PREFIX --rm cosmwasm/faucet:manual cosmos-faucet generate
